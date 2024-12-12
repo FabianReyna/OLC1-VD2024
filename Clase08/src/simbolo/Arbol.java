80cbddce018446e25dec8a145465500b20d09bc1
FabianReyna <3753836700101@ingenieria.usac.edu.gt>
2024-12-12 16:59:48 -0600

    Clase 08
    Ejemplo de ambitos (IF y FOR)

---
+++ Clase08/src/simbolo/Arbol.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simbolo;
+
+import abstracto.Instruccion;
+import excepciones.Errores;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author fabian
+ */
+public class Arbol {
+
+    private LinkedList<Instruccion> instrucciones;
+    private String consola;
+    private LinkedList<Errores> errores;
+    // tabla de simbolos (global)
+
+    public Arbol(LinkedList<Instruccion> instrucciones) {
+        this.instrucciones = instrucciones;
+        consola = "";
+        this.errores = new LinkedList<>();
+    }
+
+    public LinkedList<Instruccion> getInstrucciones() {
+        return instrucciones;
+    }
+
+    public String getConsola() {
+        return consola;
+    }
+
+    public LinkedList<Errores> getErrores() {
+        return errores;
+    }
+    
+
+    public void setInstrucciones(LinkedList<Instruccion> instrucciones) {
+        this.instrucciones = instrucciones;
+    }
+
+    public void Print(String valor) {
+        this.consola += valor + "\n";
+    }
+
+    public void AddErrores(Errores e) {
+        this.errores.add(e);
+    }
+
+}
