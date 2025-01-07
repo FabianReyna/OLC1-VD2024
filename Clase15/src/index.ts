2d119cd83eebec0ce5750fce6e48b84664d2802e
FabianReyna <3753836700101@ingenieria.usac.edu.gt>
2025-01-06 19:26:10 -0600

    Clase 15
    Jison

---
+++ Clase15/src/index.ts
@@ -0,0 +1,13 @@
+class ejemploJison {
+    start(): any {
+        try {
+            let parser = require('./analizador.js')
+            let resultado = parser.parse('evAluar{1+2};imprimir{\"hola mundo\"};evaluar{(3*5)+(1/3)};');
+            //console.log(resultado);
+    } catch (e: any) {
+            console.log(e)
+        }
+    }
+}
+export const ejemploJison2 = new ejemploJison();
+ejemploJison2.start();
